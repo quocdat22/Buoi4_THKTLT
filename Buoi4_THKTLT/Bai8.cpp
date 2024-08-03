498bebfeae309a1e5d3c59a002e447ffa4b36f5e
Hoang Le Quoc Dat <hoanglequocdat132002@gmail.com>
2024-08-03 15:46:00 +0700

    Bai8_Xuất dãy số Fibonaci mà giá trị các số nhỏ hơn m

--- Buoi4_THKTLT/Bai8.cpp
+++ Buoi4_THKTLT/Bai8.cpp
@@ -1,10 +1,29 @@
 #include "BaiTap.h"
 
 #include<stdio.h>
+int Fibonacci8(int n) {
+    if (n <= 2)
+        return 1;
+    return Fibonacci8(n - 1) + Fibonacci8(n - 2);
+}
 
+void printFibonacciLessThanM(int m) {
+    int i = 1;
+    int fib = Fibonacci8(i);
+    while (fib < m) {
+        printf("%d ", fib);
+        i++;
+        fib = Fibonacci8(i);
+    }
+    printf("\n");
+}
 
 void bai8() {
-
+    int m;
+    printf("Nhập giá trị m: ");
+    scanf_s("%d", &m);
+    printf("Dãy số Fibonacci có giá trị nhỏ hơn %d là:\n", m);
+    printFibonacciLessThanM(m);
 
 	return;
 }
